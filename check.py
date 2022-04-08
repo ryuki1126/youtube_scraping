ecab9653354b040aa427251c6a12c9d4683503b3
ryuki1126 <uv.ryuki1126@gmail.com>
2022-04-08 12:16:41 +0900

    .gitignoreファイルの追加

--- check.py
+++ check.py
@@ -1,13 +1,13 @@
 import urllib.robotparser
 
 # robots.txtの読み取り
-robots_txt_url = 'https://ytranking.net/robots.txt'
+robots_txt_url = 'https://www.biccamera.com/robots.txt'
 rp = urllib.robotparser.RobotFileParser()
 rp.set_url(robots_txt_url)
 rp.read()
 
 # robots.txtの情報から調査したいURL、User-Agentでクロール可能かを調べる
 user_agent = '*'
-url = 'https://ytranking.net/ranking/mon/*'
+url = 'https://www.biccamera.com/bc/*'
 result = rp.can_fetch(user_agent, url)
 print(result)
